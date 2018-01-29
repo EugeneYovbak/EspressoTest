71e071218e98b94fbcc01d04bfed8e1c87c930b0
Yovbak Eugene <perspikyliator@gmail.com>
2018-01-29 15:03:01 +0200

    tested getProductList() method

--- app/src/test/java/com/boost/espressotest/MainPresenterUnitTest.java
+++ app/src/test/java/com/boost/espressotest/MainPresenterUnitTest.java
@@ -1,18 +1,29 @@
 package com.boost.espressotest;
 
 import com.boost.espressotest.domain.ProductRepository;
+import com.boost.espressotest.domain.exceptions.NoConnectivityException;
+import com.boost.espressotest.domain.model.Product;
 import com.boost.espressotest.presentation.screen.main.presenter.MainPresenter;
 import com.boost.espressotest.presentation.screen.main.view.MainView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
 import io.reactivex.android.plugins.RxAndroidPlugins;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -39,5 +50,60 @@ public class MainPresenterUnitTest {
         mMainPresenter.onAttach(mMainView);
     }
 
+    @Test
+    public void getProductListSuccessTest() {
+        List<Product> productList = generateProductList();
 
+        when(mProductRepository.getProductList(1, 50))
+                .thenReturn(Observable.just(productList));
+
+        mMainPresenter.getProductList();
+
+        Mockito.verify(mMainView, times(1)).showLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).hideLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).showProducts(productList);
+    }
+
+    @Test
+    public void getProductListErrorTest() {
+        Exception exception = new Exception();
+
+        when(mProductRepository.getProductList(1, 50))
+                .thenReturn(Observable.error(exception));
+
+        mMainPresenter.getProductList();
+
+        Mockito.verify(mMainView, times(1)).showLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).hideLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).productsLoadError();
+    }
+
+    @Test
+    public void getProductListConnectionErrorTest() {
+        NoConnectivityException noConnectivityException = new NoConnectivityException();
+
+        when(mProductRepository.getProductList(1, 50))
+                .thenReturn(Observable.error(noConnectivityException));
+
+        mMainPresenter.getProductList();
+
+        Mockito.verify(mMainView, times(1)).showLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).hideLoadingIndicator();
+        Mockito.verify(mMainView, times(1)).internetConnectionError();
+    }
+
+    private List<Product> generateProductList() {
+        List<Product> productList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Product product = new Product(
+                    i,
+                    "Name" + i,
+                    i * 100,
+                    "Producer" + i,
+                    "https://picsum.photos/" + i,
+                    i % 2 == 0);
+            productList.add(product);
+        }
+        return productList;
+    }
 }
